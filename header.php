62722de27bf1fcce13d27d95365f1a31d0f6cd1d
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-29 13:06:15 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="navbar-collapse d-flex flex-row" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active hover-pocetne">
+          <a class="nav-link" href="index.php">Glavna stranica</a>
+      </li>
+      <li class="nav-item hover-pocetne">
+        <a class="nav-link" href="o_nama_jav.php">O nama</a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav navbar-right">
+      <li class="nav-item active hover-pocetne">
+        <a class="nav-link" href="registracija.php">Registracija</a>
+      </li>
+      <li class="nav-item hover-pocetne">
+        <a class="nav-link" href="verifikacija.php">Verifikacija</a>
+      </li>
+    </ul>
+  </div>
+</nav>
